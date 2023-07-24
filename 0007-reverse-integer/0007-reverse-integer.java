84cd405a47510da646b4bf22dbdb08afab74961f
Mohan Ma <112891561+Joseph941230@users.noreply.github.com>
2023-07-23 20:08:09 -0700

    Time: 1 ms (100.00%), Space: 39.9 MB (56.16%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+             int res = 0;
+        while(x != 0) {
+            int digit = x % 10;
+            int temp = res * 10 + digit;
+
+            if(temp / 10 != res){
+                return 0;
+            }
+
+            res = temp;
+            x /= 10;
+        }
+
+        return res;
+    
+    }
+}
