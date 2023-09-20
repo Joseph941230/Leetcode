8d3192011b83d38c0d1cbaaaf87ffa51f393492a
Mohan Ma <112891561+Joseph941230@users.noreply.github.com>
2023-09-20 09:49:34 -0700

    Time: 46 ms (21.35%), Space: 14.4 MB (56.70%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        if sorted(s) != sorted(t):
+            return False
+        else:
+            return True
